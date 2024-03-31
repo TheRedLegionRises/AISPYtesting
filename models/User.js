7b3d68a4d5c061cebfd1328617cf57f3d7c3dc2c
TheRedLegionRises <65699655+TheRedLegionRises@users.noreply.github.com>
2024-03-30 20:42:47 -0700

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const createUser = async (firstName, email, password) => {
 
 const findUserByEmail = async (email) => {
   try {
-    const [rows] = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
+    const rows = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
     return rows[0]; // If rows is empty, rows[0] will be undefined, which is perfectly fine
   } catch (error) {
     console.error('Error finding user by email:', error);
