cf0e5124345fe3bca6deedca3f32734ff385569c
TheRedLegionRises <65699655+TheRedLegionRises@users.noreply.github.com>
2024-03-30 21:16:26 -0700

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -28,7 +28,7 @@ const findUserByEmail = async (email) => {
       // res.statusCode = 200;
       // res.end(JSON.stringify(results));
       let parsedResults = JSON.parse(JSON.stringify(results));
-      console.log(parsedResults[0]);
+      console.log(parsedResults[0].name);
       return results;
     });
 
